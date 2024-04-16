879559f3a38851b88abf7b160093168b037b51ac
Barisscebeci <brscbc8@gmail.com>
2024-04-16 21:38:35 +0300

    düzenleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,104 @@
 import { useState } from "react";
-import "./App.css";
-
-const api = {
-  key: "8da416c57620407d91461f2664fbbcfc",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+import axios from "axios";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState({});
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  // Başlangıçta tam dosya yolu ile başlayın.
+  const [backgroundImage, setBackgroundImage] = useState('/assets/Arkaplan.jpg'); 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6b45ec28e633a07e3038b51c69990578&units=metric`;
 
-  const searchPressed = () => {
-    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setWeather(result);
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        updateBackground(response.data.name); // Arka planı güncelleme fonksiyonunu çağır
       });
+      setLocation("");
+    }
+  };
+
+  const updateBackground = (locationName) => {
+    // İlçeye göre arka plan resmini güncelle
+    if (locationName.toLowerCase() === "biga") {
+      setBackgroundImage('/assets/resim.jpg'); // Biga için özel arka plan resmi
+    } else {
+      setBackgroundImage('/assets/Arkaplan.jpg'); // Varsayılan arka plan resmi
+    }
+  };
+
+  const translateWeather = (weatherCondition) => {
+    const conditions = {
+      Clear: "Açık",
+      Clouds: "Bulutlu",
+      Rain: "Yağmurlu",
+      Snow: "Karlı",
+      Mist: "Sisli",
+      Smoke: "Dumanlı",
+      Haze: "Puslu",
+      Dust: "Tozlu",
+      Fog: "Sisli",
+      Sand: "Kumlu",
+      Ash: "Kül",
+      Squall: "Sağanak",
+      Tornado: "Kasırga",
+      Drizzle: "Çiseleyen Yağmur",
+      Thunderstorm: "Gök Gürültülü Fırtına",
+    };
+
+    return conditions[weatherCondition] || weatherCondition;
   };
 
   return (
-    <div className="App">
-      <header>
-        <h1>Hava Durumu</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter City"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button onClick={searchPressed}>Search</button>
+    <div className="app" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Lütfen Şehir Adı Giriniz"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? (
+              <h1 className="bold">{data.main.temp.toFixed()} °C</h1>
+            ) : null}
+          </div>
+          <div className="description">
+            {data.weather ? (
+              <p className="bold">{translateWeather(data.weather[0].main)}</p>
+            ) : null}
+          </div>
         </div>
-
-        {typeof weather.main != "undefined" ? (
-               <div>
-               <p>{weather.name}</p>
-               <p>{weather.main.temp}*C</p>
-               
-                 <p>{weather.weather[0].main}</p>
-                 <p>({weather.weather[0].descriptions})</p>
-               </div>
-        ): (
-        "" 
+        {data.name != undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°C</p>
+              ) : null}
+              <p>Hissedilen</p>
+            </div>
+            <div className="humidity">
+              {data.main ? (
+                <p className="bold"> {data.main.humidity}%</p>
+              ) : null}
+              <p>Nem Oranı</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Rüzgar</p>
+            </div>
+          </div>
         )}
- 
-      </header>
+      </div>
     </div>
   );
 }
